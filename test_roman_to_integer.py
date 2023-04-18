80148e9b065a7d766243f2b8b27081f8358f5d18
Peter Baintner <pb@dreisicht.net>
2023-04-18 23:52:41 +0200

    Add tests

--- test_roman_to_integer.py
+++ test_roman_to_integer.py
@@ -5,5 +5,5 @@ from roman_to_integer import Solution
 def test_solution():
   """Tests the Solution class."""
   sol_instance = Solution()
-  print(sol_instance.roman_to_int("MXI") == 1011)
-  print(sol_instance.roman_to_int("MIX") == 1009)
+  assert sol_instance.roman_to_int("MXI") == 1011
+  assert sol_instance.roman_to_int("MIX") == 1009
